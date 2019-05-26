5938443add5bc90d4c3988875a82b54e229c546b
Aswin <aswinkarthik.rv@gmail.com>
2019-05-26 11:57:38 +0200

    staging commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask import jsonify
+from time import sleep
+from subprocess import Popen, PIPE, call
+
+import threading
+
+app = Flask(__name__)
+Bootstrap(app)
+
+ready = True
+output = ""
+err = ""
+
+
+def download_task(playlist, destination):
+    global ready, output, err
+    ready = False
+    p = Popen(['spotdl', '--playlist', playlist], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+
+    txt_file = str(err).split(' ')[-1][0:-3]  # txt file to download and delete
+    rc = p.returncode
+
+    p = Popen(['spotdl', '--list', txt_file, '-f', '/root/Music/'+destination], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    # p = call(['spotdl', '-l', txt_file, '-f', destination])
+    call(['rm', txt_file], shell=True)
+
+    ready = True
+
+@ app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('app.html', ready=ready, output=str(output), err=str(err))
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def start_download():
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        x = threading.Thread(target=download_task,
+                             args=[request.form.get('playlist'), request.form.get('destination')])
+        x.start()
+        return render_template('app.html', message='POST')
+
+
+if __name__ == '__main__':
+    app.run()
